460b48e74240c59c92664d9995368ec0b81b6412
yazdog8 <joe@pauilsenweb.com>
2019-07-25 22:44:58 -0700

    Create MUI theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react"
 import { Router, Route, Switch } from "react-router-dom"
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
 import history from "./data/history"
 import { DEFAULT, DASHBOARD } from "./common/constants/routingConstants"
 
 function App() {
+  const muiTheme = createMuiTheme({
+    palette: {
+      type: "light",
+    },
+  })
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact path={DEFAULT} render={() => <h1>Default</h1>} />
-        <Route path={DASHBOARD} render={() => <h1>Dashboard</h1>} />
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={muiTheme}>
+      <Router history={history}>
+        <CssBaseline />
+        <Switch>
+          <Route exact path={DEFAULT} render={() => <h1>Default</h1>} />
+          <Route path={DASHBOARD} render={() => <h1>Dashboard</h1>} />
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   )
 }
 
